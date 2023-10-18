100ae30fd7f66135d870549efd44f25e637890c7
ris04hit <ris04hit@gmail.com>
2023-10-18 14:17:10 +0000

    Debugging phase

--- a3/client.py
+++ a3/client.py
@@ -43,17 +43,35 @@ def reply_parser(message: str):     # Converts server reply into dictionary
         parsed_msg['Data'] = data
     return parsed_msg
 
-def retransmit(message: str, received: list, index: int):       # Retransmits message
-    while not received[index]:
+def modify_rtt(time_val: float):        # Changes time parameters of program
+    def temp_modify_rtt(time_val: float):
+        global timeout_time
+        global avg_rtt
+        global dev_rtt
+        dev_rtt = 0.75*dev_rtt + 0.25*abs(time_val - avg_rtt)
+        if avg_rtt:
+            avg_rtt = 0.875*avg_rtt + 0.125*time_val
+        else:
+            avg_rtt = time_val
+        lock = threading.Lock()
+        with lock:
+            timeout_time = timeout_multiplier*(avg_rtt + 4*dev_rtt)
+        print(time_val, avg_rtt, dev_rtt, timeout_time)
+    rtt_thread = threading.Thread(target=temp_modify_rtt, args=(time_val,))
+    rtt_thread.start()
+    rtt_thread.join()
+
+def retransmit(message: str, received: list):       # Retransmits message
+    while not received[0]:
         clientsocket.sendto(message.encode(), server)
-        sleep(time_period)
+        sleep(timeout_time)
 
 def ask_size():         # Asking for size
     global data_size
     # message = 'SendSize\n\n'            # For tournament mode
     message = 'SendSize\nReset\n\n'     # For non tournament mode
     received = [False]
-    retransmit_thread = threading.Thread(target=retransmit, args = (message, received, 0))
+    retransmit_thread = threading.Thread(target=retransmit, args = (message, received))
     retransmit_thread.start()
     data = {}
     while 'Size' not in data:
@@ -68,43 +86,95 @@ def ask_size():         # Asking for size
 def data_receiver():     # Receiver for data
     global received_packet_num
     global duplicate_packet_num
+    global squished
     while received_packet_num != len(data_received):
         message = f'Offset: {data_size}\nNumBytes: {data_per_request}\n\n'
         parsed_reply = {}
         while ('Offset' not in parsed_reply) or ('Data' not in parsed_reply):
             reply, server_address = clientsocket.recvfrom(sys.getsizeof(message) + data_per_request)
+            end_time = time()
             parsed_reply = reply_parser(reply.decode())
-        lock = threading.Lock()
+        if 'Squished' in parsed_reply:
+            lock_squish = threading.Lock()
+            with lock_squish:
+                squished += 1
         received_offset = int(parsed_reply['Offset'])
         received_index = received_offset//data_per_request
+        lock = threading.Lock()
+        while not start_time[received_index]:
+            pass
+        if start_time[received_index] != -1:
+            modify_rtt(end_time-start_time[received_index])
         if not data_received[received_index]:
             with lock:
-                data_received[received_index] = reply_parser(reply.decode())['Data']
+                data_received[received_index] = parsed_reply['Data']
                 received_packet_num += 1
         else:
-            duplicate_packet_num += 1
+            with lock:
+                duplicate_packet_num += 1
 
 def send_request():        # Sends request to server
+    global requested_packet_num
     # Sending first request
     first_not_ack = 0       # index of first element in window whose data is not received
-    for ind in range(window_size):
-        offset = ind*data_per_request
-        message = f'Offset: {offset}\nNumBytes: {data_per_request}\n\n'
-        clientsocket.sendto(message.encode(), server)
     while first_not_ack < len(data_received):
-        start_timer = time()
-        offset = first_not_ack*data_per_request
-        while not data_received[first_not_ack]:
-            if time() - start_timer > time_period:      # Timeout
+        ind = first_not_ack
+        count = 0
+        while (count < window_size) and (ind < len(data_received)):
+            if not data_received[ind]:
+                offset = ind*data_per_request
                 message = f'Offset: {offset}\nNumBytes: {data_per_request}\n\n'
+                start_time_lock = threading.Lock()
+                start_time_val = time()
                 clientsocket.sendto(message.encode(), server)
-                start_timer = time()
-            sleep(time_period/num_check)
-        first_not_ack += 1
-        if (first_not_ack + offset) < len(data_received):       # Sending request for newly transmitted msg
-            offset = (first_not_ack + offset)*data_per_request
-            message = f'Offset: {offset}\nNumBytes: {data_per_request}\n\n'
-            clientsocket.sendto(message.encode(), server)
+                with start_time_lock:
+                    if not start_time[ind]:
+                        start_time[ind] = start_time_val
+                    else:
+                        start_time[ind] = -1
+                    requested_packet_num += 1
+                count += 1
+            elif count == 0:
+                first_not_ack += 1
+            ind += 1
+        sleep(timeout_time)
+
+        
+    # for ind in range(window_size):
+    #     offset = ind*data_per_request
+    #     message = f'Offset: {offset}\nNumBytes: {data_per_request}\n\n'
+    #     start_time_lock = threading.Lock()
+    #     start_time_val = time()
+    #     clientsocket.sendto(message.encode(), server)
+    #     with start_time_lock:
+    #         start_time[ind] = start_time_val
+    #         requested_packet_num += 1
+    # while first_not_ack < len(data_received):
+    #     start_timer = time()
+    #     offset = first_not_ack*data_per_request
+    #     timeout_time_local = timeout_time
+    #     while not data_received[first_not_ack]:
+    #         if time() - start_timer > timeout_time_local:      # Timeout
+    #             message = f'Offset: {offset}\nNumBytes: {data_per_request}\n\n'
+    #             start_time_lock = threading.Lock()
+    #             start_time_val = -1
+    #             clientsocket.sendto(message.encode(), server)
+    #             with start_time_lock:
+    #                 start_time[first_not_ack] = start_time_val
+    #                 requested_packet_num += 1
+    #             start_timer = time()
+    #             timeout_time_local *= 2
+    #         sleep(timeout_time_local/num_check)
+    #     if (first_not_ack + window_size) < len(data_received):       # Sending request for newly transmitted msg
+    #         offset = (first_not_ack + window_size)*data_per_request
+    #         message = f'Offset: {offset}\nNumBytes: {data_per_request}\n\n'
+    #         start_time_lock = threading.Lock()
+    #         start_time_val = time()
+    #         clientsocket.sendto(message.encode(), server)
+    #         with start_time_lock:
+    #             start_time[first_not_ack + window_size] = start_time_val
+    #             requested_packet_num += 1
+    #     first_not_ack += 1
 
 def submit_data():      # Submit data
     global result
@@ -114,7 +184,7 @@ def submit_data():      # Submit data
     md5_hex = md5_hash.hexdigest()
     message = f'Submit: {user}\nMD5: {md5_hex}\n\n'
     received = [False]
-    retransmit_thread = threading.Thread(target=retransmit, args = (message, received, 0))
+    retransmit_thread = threading.Thread(target=retransmit, args = (message, received))
     retransmit_thread.start()
     data = {}
     while 'Result' not in data:
@@ -131,8 +201,8 @@ def submit_data():      # Submit data
 # Submitting user
 user = '2021CS10547@nothing'
 
-# Vayu Server
-server_ip = '10.237.26.109'
+# Vayu Server*
+server_ip = '10.17.7.134'
 server_port = int(sys.argv[1])
 
 # Local Server
@@ -144,13 +214,16 @@ server = (server_ip, server_port)
 clientsocket = socket(AF_INET, SOCK_DGRAM)
 
 # Fixing Time Period
-time_period = 0.005
-num_check = 100     # Checks this many times whether data received before time_out
+timeout_multiplier = 5
+timeout_time = 0.1
+# num_check = 100     # Checks this many times whether data received before time_out
+avg_rtt = 0
+dev_rtt = 0
 
 # Size of data to be received
 data_size = 0
 data_per_request = 1448
-window_size = 1<<5
+window_size = 1
 
 result = False
 
@@ -162,9 +235,14 @@ while not result:
     
     #  Initializing required variables
     data_received = [None for offset in range(0, data_size, data_per_request)]
-    window_size = min(window_size, data_size//data_per_request)
+    start_time = [None for offset in range(0, data_size, data_per_request)]
+    window_size = min(window_size, len(data_received))
+    ssthresh = 0
+    congestion_state = 0        # 0: slow start     1: congestion avoidance
+    requested_packet_num = 0
     received_packet_num = 0
     duplicate_packet_num = 0
+    squished = 0
 
     # Turning on data receiver
     receiver_thread = threading.Thread(target=data_receiver)
@@ -183,7 +261,7 @@ while not result:
     submit_thread.start()
     submit_thread.join()
     
-    print(received_packet_num, duplicate_packet_num)
+    print(f'Requested Packets:\t\t{requested_packet_num}\nReceived Packets:\t\t{received_packet_num}\nDuplicate Packets:\t\t{duplicate_packet_num}\nSquished Packets:\t\t{squished}\nTime Period:\t\t\t{timeout_time}')
 
 # Closing Socket
 clientsocket.close()
